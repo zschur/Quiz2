b2cb40a7a6c0b7008eb3dce382e29d5a795b85fe
zschur <zachschur@gmail.com>
2016-10-26 20:15:15 -0400

    commit

---
+++ Tests.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import unittest
+import os
+
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        if os.name=='nt':
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = webdriver.Firefox()
